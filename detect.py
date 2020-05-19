42734f9b13c929817c9592ed5a6e5f7cca6d9f6b
John Ao <32444220+John-Ao@users.noreply.github.com>
2020-05-19 22:01:26 +0800

    Add area debug

--- detect.py
+++ detect.py
@@ -8,7 +8,7 @@ frame_rate = cap.get(cv2.CAP_PROP_FPS)
 _, last_frame = cap.read()
 h, w, _ = last_frame.shape
 out = cv2.VideoWriter(
-    "output.mp4", cv2.VideoWriter_fourcc(*"MJPG"), frame_rate, (w, h))
+    "output.mp4", cv2.VideoWriter_fourcc(*"MJPG"), frame_rate, (w*2, h))
 
 img_cnt = 0
 
@@ -33,28 +33,29 @@ def diff(a, b):
     b = np.sum(b, 2)/3
     r = np.zeros(b.shape, np.uint8)
     r[(b > a+5)] = 255
-    kernel = np.ones((5, 5), np.uint8)
     # r = cv2.morphologyEx(r, cv2.MORPH_CLOSE, kernel)
-    r = cv2.morphologyEx(r, cv2.MORPH_OPEN, kernel)
+    r = cv2.morphologyEx(r, cv2.MORPH_CLOSE, np.ones((3, 3), np.uint8))
+    r = cv2.morphologyEx(r, cv2.MORPH_OPEN, np.ones((5, 5), np.uint8))
     # return r
     # retval,labels=cv2.connectedComponents(r)
-    retval, labels, stats, centroids = cv2.connectedComponentsWithStats(r)
+    r3 = np.repeat(r.reshape(1, -1), 3).reshape(*r.shape, 3)
+    retval, _, stats, _ = cv2.connectedComponentsWithStats(r)
     for i in range(1, retval):
         l, t, w, h, area = stats[i]
-        if area > w*h*0.7 and h > 1.2*w and w > 5:
+        r3 = cv2.putText(r3, '%.3f' % (area/w/h), (l, t), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        if area > w*h*0.5 and w > 3:
             region = b[t:t+h, l:l+w]
             fregion = region.astype(np.float32)
             m = np.mean(fregion)
             v = np.var(fregion)
             if m > thres and v > 1 and np.all(region[[0, 0, -1, -1], [0, -1, 0, -1]] < m):
-                cv2.imwrite(f'sample/{img_cnt}.png', img[t: t+h, l: l+w, :])
-                img = drawbox(img, l, t, w, h)
+                # cv2.imwrite(f'sample/{img_cnt}.png', img[t: t+h, l: l+w, :])
+                img = drawbox(img, l, t, w, h, (0, 255, 0))
                 print(img_cnt, m, v)
                 img_cnt += 1
-            # else:
-            #     img = drawbox(img, l, t, w, h, (0, 255, 0))
-
-    return img
+            else:
+                img = drawbox(img, l, t, w, h, (0, 0, 255))
+    return np.concatenate([r3, img], 1)
 
 
 cnt = 0
@@ -64,7 +65,7 @@ while True:
         break
     res = diff(frame, last_frame)
     out.write(res)
-    cv2.imshow('diff', cv2.resize(res, (640, 400)))
+    cv2.imshow('diff', cv2.resize(res, (800, 640)))
     # print(cnt)
     cnt += 1
     cv2.waitKey(1)
